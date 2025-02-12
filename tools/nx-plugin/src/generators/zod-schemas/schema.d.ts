4a40f655be76cad09d87a74f817ee91271e4c6a2
Mateusz Jonak <mateusz@web.blue>
2025-02-12 16:26:32 +0100

    fix: eslint dependency issue

--- tools/nx-plugin/src/generators/zod-schemas/schema.d.ts
+++ tools/nx-plugin/src/generators/zod-schemas/schema.d.ts
@@ -1,5 +1,20 @@
+/**
+ * Defines the options interface for your Nx generator.
+ */
 export interface ZodSchemasGeneratorOptions {
-  inputPath: string;
+  /**
+   * The name of your Nx project/library.
+   * e.g. 'my-zod-lib'
+   */
   libraryName: string;
+
+  /**
+   * The directory where .blue files exist (usually 'blue-preprocessed/SomeModule').
+   */
+  inputPath: string;
+
+  /**
+   * Skip Nx/Prettier formatting after generation.
+   */
   skipFormat?: boolean;
 }
